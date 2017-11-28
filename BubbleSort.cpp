a9d8ca8df58f6b1e4c17d2680cf2fb48e099ae01
JhonVe7 <32178848+JhonVe7@users.noreply.github.com>
2017-11-28 16:03:30 -0500

    Update BubbleSort.cpp

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -1,119 +1,42 @@
-
 #include <iostream>
-
-
-
-
 #include <conio.h>
-
-
-
-
-
-
+/*
+*
+*TALLER 4 - Estructura de Datos- punto 1
+*	@author JhonVe7
+*	@date 25-11-2017
+*	@version 2
+*
+*/
 using namespace std;
 
-
-int main()
-
-
-{
-
-
-   int vector[5];
-
-
-   int i, j , temp;
-
-
-
-
-
-
-   cout<<" Ingrese los numeros a ordenar "<<endl;
-
-
-   {
-
-
-       for(i=0;i<5;i++)
-
-
-       {
-
-
-           cout<<i+1<<" -> ";
-
-
-           cin>>vector[i];
-
-
-           cout<<endl;
-
-
-       }
-
-
+int main(){
+//Define el tamaño del vector 
+    int n, temp;
+cout<<" Ingrese el tamaño del vector "<<endl;    
+cin >> n;
+int vector[n];
+cout<<" Ingrese los numeros a ordenar "<<endl;
+   for(int i=0;i<n;i++){
+      cin>>vector[i];
+      cout<<i+1<<" -> ";
+      cout<<endl;
    }
-
-
-
-
-
-
-   for(i=0;i<5;i++)
-
-
-   {
-
-
-       for(j=i+1;j<5;j++)
-
-
-       {
-
-
-           if(vector[j]<vector[i])
-
-
-           {
-
-
+}
+// metodo de intecambio por medio de un aux
+for(int i=0;i<n;i++){
+   for(j=i+1;j<n;j++){
+           if(vector[j]<vector[i]){
                temp=vector[j];
-
-
                vector[j]=vector[i];
-
-
-               vector[i]=vector[j];
-
-
                vector[i]=temp;
-
-
            }
-
-
        }
-
-
    }
-
-
-   for(i=0;i<5;i++)
-
-
-   {
-
-
+// imprime los numeros del vector
+ for(i=0;i<n;i++){
        cout<<vector[i]<<" ";
-
-
    }
-
-
    return 0;
-
-
 }
 
